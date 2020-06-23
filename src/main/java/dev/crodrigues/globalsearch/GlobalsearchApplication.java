4031d88d837d01df74a023f8a0db8663b3f607db
Carlos Rodrigues <krezeberto@gmail.com>
2020-06-23 14:27:12 -0300

    :tada: Initial commit

---
+++ src/main/java/dev/crodrigues/globalsearch/GlobalsearchApplication.java
@@ -0,0 +1,13 @@
+package dev.crodrigues.globalsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/crodrigues/globalsearch/GlobalsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.crodrigues.globalsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
